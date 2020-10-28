bc28113efac160f3b88a49f48505c831c7c89606
dannyelgjl <dannyelgjl@gmail.com>
2020-10-28 20:12:57 -0300

    adicionando os números dos Pins

--- src/screens/PinScreen/index.js
+++ src/screens/PinScreen/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Fontisto } from '@expo/vector-icons'
+
+import NumberPad from '../../components/NumberPad';
 
 import Text from '../../components/Text'
 
 const PinScreen = ({ navigation }) => {
+  const [pinCount, setPinCount] = useState(0);
+  
+  const totalPins = 6;
+
+  const renderPins = () => {
+    const pins = [];
+
+    for (let x = 1; x <= totalPins; x++) {
+      pins.push(
+        x <= pinCount ? (
+          <PinContainer key={x}>
+            <Pin />
+          </PinContainer>
+        ) : (
+          <PinContainer key={x}/>
+        )
+      );
+    }
+    return pins;
+  };
+
+  const pressKey = (_, index) => {
+
+  }
+
   return (
       <Container>
         <Text center heavy title color="#964ff0" margin="32px 0 0 0" >MyBank</Text>
         <Text center heavy medium margin="32px 0 0 0" >Enter your PIN code.</Text>
 
         <AccessPin>
-
+          {renderPins()}
         </AccessPin>
 
         <Text center bold margin="8px 0 0 0" color="#9c9c9f" >Forgot PIN?</Text>
@@ -20,6 +47,8 @@ const PinScreen = ({ navigation }) => {
           <Fontisto name="locked" color="#964ff0" size={16} />
           <Text bold margin="0 0 0 8px" color="#964ff0">Use Touch ID</Text>
         </UseTouch>
+
+        <NumberPad onPress={pressKey}/>
 
         <StatusBar barStyle="light-content" />      
       </Container>
@@ -34,7 +63,9 @@ const Container = styled.SafeAreaView`
 `;
 
 const AccessPin = styled.View`
-
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 32px 64px 16px 64px
 `;
 
 const UseTouch = styled.TouchableOpacity`
@@ -44,6 +75,26 @@ const UseTouch = styled.TouchableOpacity`
   justify-content: center;
 `;
 
+
+const PinContainer = styled.View`
+  width: 16px;
+  height: 16px;
+  border-radius: 8px;
+  border-width: 1px;
+  border-color: #5196f4;
+  align-items: center;
+  justify-content: center;
+
+`;
+
+const Pin = styled.View`
+  width: 8px;
+  height: 8px;
+  border-radius: 4px;
+  background-color: #5196f4;
+`;
+
 const StatusBar = styled.StatusBar`
 
 `;
+
